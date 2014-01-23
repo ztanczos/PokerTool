42e30446c293fbe5e383b58f1aba60856fabfd9f
Zoltan Tanczos <ztanczos@gmail.com>
2014-01-23 14:48:10 +0100

    slight layout change

---
+++ app/js/filters/group.js
@@ -0,0 +1,16 @@
+'use strict';
+
+pokerApp.filter('group', function () {
+    return function (items, groupSize) {
+        var groups = [],
+            inner;
+        for(var i = 0; i < items.length; i++) {
+            if(i % groupSize === 0) {
+                inner = [];
+                groups.push(inner);
+            }
+            inner.push(items[i]);
+        }
+        return groups;
+    }
+})
